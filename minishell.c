7d88555ce72e919610258405928e0cc80e71ee3d
Francesco fortunato Gradia <fgradia@e4r6p4.42roma.it>
2021-05-25 19:58:13 +0200

    25.05.21 - better read: buf to list_node

--- minishell.c
+++ minishell.c
@@ -1,13 +1,13 @@
 #include "my_minishell.h"
 
-int	ft_strchr(char *str, char c)
+int	ft_strchr(char c, char *s1)
 {
 	int	x;
 
 	x = 0;
-	while (str[x])
+	while (s1 && s1[x])
 	{
-		if (str[x] == c)
+		if (s1[x] == c)
 			return (x);
 		x++;
 	}
@@ -68,14 +68,14 @@ void	ft_read(t_list **head)
 	free(buf);
 }
 */
-
+/*
 int	ft_read(t_list **head)
 {
 	int		r;
 
-	(*head)->cmd = (char *)malloc(sizeof(char) * 1024);
-	(*head)->cmd[1023] = '\0';
-	r = read(0, (*head)->cmd, 1023);
+	(*head)->cmd = (char *)malloc(sizeof(char) * 10240);
+	(*head)->cmd[10239] = '\0';
+	r = read(0, (*head)->cmd, 10239);
 	if (r > 1)
 		(*head)->cmd[r - 1] = '\0';
 	if ((*head)->cmd[0] == '\0')
@@ -101,10 +101,154 @@ void	ft_no_arg()
 	}
 	ft_write("exit\n");
 }
+*/
+
+void	ft_free_read(t_read **head)
+{
+	t_read	*tmp;
+
+	while (*head)
+	{
+		tmp = *head;
+		*head = (*head)->next;
+		free(tmp->line);
+		free(tmp);
+	}
+}
+
+void	ft_exit(char *str, t_data *data)
+{
+	ft_write(str);
+	ft_free_read(&data->cmd_head);
+	free(data->cmd_head);
+//	printf("xxxxxxxxxxx\n");
+	read(0, &str, 1);
+	exit(0);
+}
+
+int	ft_buf_to_node(int len, int x, char *buf, t_read *new)
+{
+	int	y;
+
+	y = 0;
+//	if (!ft_check_buf(buf + x, len))
+//		return (-1);
+	new->line = (char *)malloc(sizeof(char) * (len + 1));
+	new->line[len] = '\0';
+	while (buf[x] != '\n' && buf[x] != '\0')
+	{
+		new->line[y++] = buf[x];
+		x++;
+	}
+	new->line[y] = '\0';
+	new->next = NULL;
+	x++;
+	return (x);
+}
+
+void	ft_append_read(t_read *new, t_read **head)
+{
+	t_read	*tmp;
+
+	tmp = *head;
+	if (*head == NULL)
+		*head = new;
+	else
+	{
+		while (tmp->next)
+			tmp = tmp->next;
+		tmp->next = new;
+	}
+}
+
+void	ft_buf_to_list(char *buf, t_data *data)
+{
+	int		len;
+	int		x;
+	t_read	*new;
+
+	x = 0;
+	len = 0;
+	while (buf && buf[x])
+	{
+		len = ft_strchr('\n', buf + x);
+		if (len < 0)
+			break ;
+		//else if (len > 3 || len < 2)
+		//	ft_exit("Error: wrong op!\n", data);
+		new = (t_read *)malloc(sizeof(t_read) * 1);
+		x = ft_buf_to_node(len, x, buf, new);
+	//	if (x < 0)
+	//		ft_exit("Error:!\n", data);
+		ft_append_read(new, &data->cmd_head);
+		new = new->next;
+	}
+}
+
+void	ft_read_ops(t_data *data)
+{
+	char	*buf;
+	int		r;
+
+	buf = (char *)malloc(sizeof(char) * 1024);
+	buf[1023] = '\0';
+	r = 1;
+	while (r > 0)
+	{
+		r = read(0, buf, 1023);
+		buf[r] = '\0';
+		if (buf[0] == '\0')
+		{
+			free(buf);
+			return ;
+		}
+		ft_buf_to_list(buf, data);
+		ft_write("minishell% ");
+	}
+	free(buf);
+}
+
+void	ft_print_cmd(t_read **head)
+{
+	t_read *tmp;
+
+	tmp = *head;
+	while (tmp)
+	{
+		printf("%s\n", tmp->line);
+		tmp = tmp->next;
+	}
+}
+void	ft_no_arg(t_data *data)
+{
+//	t_list	*head;
+//	int		r;
+
+//	head = (t_list *)malloc(sizeof(t_list) * 1);
+//	head->cmd = (char *)malloc(sizeof(char) * 1024);
+//	head->cmd[1023] = '\0';
+//	r = 1;
+//	while (r)
+//	{
+		ft_write("minishell% ");
+	//	r =
+	 ft_read_ops(data);
+	//	printf("\tcmd: -%s-\n", head->cmd);
+		//ft_exec_cmd();
+	//}
+	ft_write("exit\n");
+	ft_print_cmd(&data->cmd_head);
+}
+
 
 int	main(int ac, char **av)
 {
+	t_data	data;
+
+	data.cmd_head = (t_read *)malloc(sizeof(t_read) * 1);
+	data.cmd_head = NULL;
 	if (ac == 1)
-		ft_no_arg();
+		ft_no_arg(&data);
 	(void)av;
+	ft_exit("", &data);
 }
--- my_minishell.h
+++ my_minishell.h
@@ -4,17 +4,28 @@
 # include <unistd.h>
 # include <stdlib.h>
 # include <stdio.h>
-
+/*
 typedef struct s_list
 {
 	char			*cmd;
 	struct s_list	*next;
 }	t_list;
+*/
+typedef struct s_read
+{
+	char			*line;
+	struct s_read	*next;
+}	t_read;
+
+typedef struct	s_data
+{
+	t_read	*cmd_head;
+}	t_data;
 
 /*		minishell.c		*/
 int		ft_strlen(char *str);
 void	ft_write(char *str);
 //void	ft_read(t_list **head);
-void	ft_no_arg();
+void	ft_no_arg(t_data *data);
 
 #endif
