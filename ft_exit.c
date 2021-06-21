01452a39bd82e3dac58fd9178b4224f25d1d1f70
fgradia <fgradia@student.42roma.it>
2021-06-21 10:17:30 +0000

    fixxing

--- ft_exit.c
+++ ft_exit.c
@@ -35,6 +35,7 @@ void	ft_exit(char *str, t_data *data)
 		ft_free_read(data->env_head);
 	//	ft_free_env_read(data->env_head);
 	free(data->env_head);
+	free(data->envp);
 	tcsetattr(0, 0, &data->old_term);
 	printf("xxxxxxxxxxx\n");
 	printf("waiting_leaks\n");
